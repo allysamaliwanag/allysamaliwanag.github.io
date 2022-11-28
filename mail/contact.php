9c60990e06cb9df8791fb455a138d833638e5c31
lleymm <maliwanagallysa@gmail.com>
2022-11-28 17:16:21 +0800

    Update

--- mail/contact.php
+++ mail/contact.php
@@ -14,6 +14,6 @@ $body = "You have received a new message from your website contact form.\n\n"."H
 $header = "From: $email";
 $header .= "Reply-To: $email";	
 
-if(!mail($to, $subject, $body, $header))
+if(!mail($to, $email, $body, $header))
   http_response_code(500);
 ?>
