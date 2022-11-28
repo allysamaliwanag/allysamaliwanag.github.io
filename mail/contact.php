b57bb61286bd3bb5e5b2485fb4798fe1c8bbfc57
lleymm <maliwanagallysa@gmail.com>
2022-11-28 17:05:18 +0800

    Update

--- mail/contact.php
+++ mail/contact.php
@@ -9,11 +9,11 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 $to = "maliwanagallysa@gmail.com"; // Change this email to your //
-$subject = "$email_name:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\n Message: $message";
+$email = "$message_message:  $name";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nMessage: $message";
 $header = "From: $email";
 $header .= "Reply-To: $email";	
 
-if(!mail($to, $body, $header))
+if(!mail($to, $subject, $body, $header))
   http_response_code(500);
 ?>
